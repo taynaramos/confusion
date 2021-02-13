33a2cdbf941a5e9566f53694d6e8ef7adcb0a612
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-02-13 13:08:42 -0300

    Assignment 3 - solution

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,41 +1,137 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
+    Button, Modal, ModalHeader, ModalBody, Label, Col, FormGroup
+} from 'reactstrap';
 import { Link } from 'react-router-dom'
-
+import { Control, LocalForm, Errors } from 'react-redux-form'
 
 function RenderDish({ dish }) {
     return (
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle> {dish.name} </CardTitle>
-                    <CardText> {dish.description} </CardText>
-                </CardBody>
-            </Card>
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle> {dish.name} </CardTitle>
+                <CardText> {dish.description} </CardText>
+            </CardBody>
+        </Card>
     )
 }
 
-function RenderComments({ comments }) {
-    if (comments != null)
-        return (
-            <>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {comments.map((comment) => {
-                        return (
-                            <li key={comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </>
-        )
-    else
-        return (
-            <div></div>
-        );
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+class RenderComments extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        console.log("Current State is: " + JSON.stringify(values))
+        alert("Current State is: " + JSON.stringify(values))
+    }
+
+    render() {
+        if (this.props.comments != null)
+            return (
+                <>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {this.props.comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <Button outline onClick={this.toggleModal}>
+                        <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                </Button>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                                <FormGroup>
+                                <Label htmlFor="rating" md={10}>Rating</Label>
+                                <Col md={10}>
+                                    <Control.select
+                                        model=".rating"
+                                        name="rating"
+                                        className="form-control"
+                                    >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="author" md={10}>Your Name</Label>
+                                    <Col md={10}>
+                                        <Control.text
+                                            model=".author"
+                                            id="author"
+                                            name="author"
+                                            placeholder="Your Name"
+                                            className="form-control"
+                                            innerRef={(input) => this.author = input} // no reactstrap o innerRef equivale a ref
+                                            validators={{
+                                                minLength: minLength(3),
+                                                maxLength: maxLength(15)
+                                            }}
+                                        ></Control.text>
+                                        <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            minLength: "Must be greater than 2 characters",
+                                            maxLength: "Must be 15 characters or less"
+                                        }}
+                                    />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="comment" md={10}>Comment</Label>
+                                    <Col md={10}>
+                                        <Control.textarea
+                                            model=".comment"
+                                            id="comment"
+                                            name="comment"
+                                            rows="6"
+                                            className="form-control"
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <Button type="submit" value="submit" color="primary">
+                                    Submit
+                            </Button>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </>
+            )
+        else
+            return (
+                <div></div>
+            );
+    }
 }
 
 const DishDetail = (props) => {
