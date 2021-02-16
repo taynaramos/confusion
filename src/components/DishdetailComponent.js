f278db3aab6ab281c17de27678486e27cbeffcd8
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-02-16 02:38:20 -0300

    Redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,8 +20,33 @@ function RenderDish({ dish }) {
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
-class RenderComments extends React.Component {
 
+function RenderComments({ comments, addComment, dishId }) {
+    if (comments != null)
+        return (
+            <React.Fragment>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <CommentForm dishId={dishId} addComment={addComment} />
+            </React.Fragment>
+        )
+    else
+        return (
+            <div></div>
+        );
+
+}
+
+class CommentForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -39,33 +64,20 @@ class RenderComments extends React.Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log("Current State is: " + JSON.stringify(values))
-        alert("Current State is: " + JSON.stringify(values))
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
     }
 
     render() {
-        if (this.props.comments != null)
-            return (
-                <>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {this.props.comments.map((comment) => {
-                            return (
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    <Button outline onClick={this.toggleModal}>
-                        <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        return (
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil fa-lg"></span> Submit Comment
                 </Button>
-                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-                        <ModalBody>
-                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                                <FormGroup>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <FormGroup>
                                 <Label htmlFor="rating" md={10}>Rating</Label>
                                 <Col md={10}>
                                     <Control.select
@@ -80,23 +92,23 @@ class RenderComments extends React.Component {
                                         <option>5</option>
                                     </Control.select>
                                 </Col>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor="author" md={10}>Your Name</Label>
-                                    <Col md={10}>
-                                        <Control.text
-                                            model=".author"
-                                            id="author"
-                                            name="author"
-                                            placeholder="Your Name"
-                                            className="form-control"
-                                            innerRef={(input) => this.author = input} // no reactstrap o innerRef equivale a ref
-                                            validators={{
-                                                minLength: minLength(3),
-                                                maxLength: maxLength(15)
-                                            }}
-                                        ></Control.text>
-                                        <Errors
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="author" md={10}>Your Name</Label>
+                                <Col md={10}>
+                                    <Control.text
+                                        model=".author"
+                                        id="author"
+                                        name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        innerRef={(input) => this.author = input} // no reactstrap o innerRef equivale a ref
+                                        validators={{
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    ></Control.text>
+                                    <Errors
                                         className="text-danger"
                                         model=".author"
                                         show="touched"
@@ -105,32 +117,28 @@ class RenderComments extends React.Component {
                                             maxLength: "Must be 15 characters or less"
                                         }}
                                     />
-                                    </Col>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor="comment" md={10}>Comment</Label>
-                                    <Col md={10}>
-                                        <Control.textarea
-                                            model=".comment"
-                                            id="comment"
-                                            name="comment"
-                                            rows="6"
-                                            className="form-control"
-                                        />
-                                    </Col>
-                                </FormGroup>
-                                <Button type="submit" value="submit" color="primary">
-                                    Submit
+                                </Col>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="comment" md={10}>Comment</Label>
+                                <Col md={10}>
+                                    <Control.textarea
+                                        model=".comment"
+                                        id="comment"
+                                        name="comment"
+                                        rows="6"
+                                        className="form-control"
+                                    />
+                                </Col>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">
+                                Submit
                             </Button>
-                            </LocalForm>
-                        </ModalBody>
-                    </Modal>
-                </>
-            )
-        else
-            return (
-                <div></div>
-            );
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        )
     }
 }
 
@@ -154,7 +162,9 @@ const DishDetail = (props) => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
                     </div>
                 </div>
             </div>
