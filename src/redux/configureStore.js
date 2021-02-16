9e826695c00a2db1a89f79751ace75c890db76d4
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-02-16 01:35:40 -0300

    Combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux'
-import { Reducer, initialState } from './reducer'
+// use combineReducers to combine smaller reducers into one
+import { createStore, combineReducers } from 'redux'
+
+// smaller reducers being imported
+import { Dishes } from './dishes'
+import { Comments } from './comments'
+import { Promotions } from './promotions'
+import { Leaders } from './leaders'
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            // what is managed: who is managing
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
