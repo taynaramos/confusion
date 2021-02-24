c24816ca38150ee3fc98ff35208b51438572ad4b
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-02-24 17:31:46 -0300

    Fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import {
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent'
+import { baseUrl } from '../shared/baseUrl'
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -13,7 +14,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 function RenderDish({ dish }) {
     return (
         <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle> {dish.name} </CardTitle>
                 <CardText> {dish.description} </CardText>
